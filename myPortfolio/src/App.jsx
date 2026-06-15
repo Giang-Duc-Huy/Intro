aa95706e4be6b59dee27424e13ff6407f87980f5
Giang-Duc-Huy <giangduchuy537@gmail.com>
2026-06-15 22:14:47 +0700

    change files

--- myPortfolio/src/App.jsx
+++ myPortfolio/src/App.jsx
@@ -1,10 +1,27 @@
-const App = () => {
-  return (
-    <div>
-      <h1>My Portfolio</h1>
-      <p>Welcome to my portfolio website! Here you can find information about my projects, skills, and experience.</p>  
-    </div>
-  )
-}
+import Testimonials from "./sections/Testimonials";
+import Footer from "./sections/Footer";
+import Contact from "./sections/Contact";
+import TechStack from "./sections/TechStack";
+import Experience from "./sections/Experience";
+import Hero from "./sections/Hero";
+import ShowcaseSection from "./sections/ShowcaseSection";
+import LogoShowcase from "./sections/LogoShowcase";
+import FeatureCards from "./sections/FeatureCards";
+import Navbar from "./components/NavBar";
 
-export default App
+const App = () => (
+  <>
+    <Navbar />
+    <Hero />
+    <ShowcaseSection />
+    <LogoShowcase />
+    <FeatureCards />
+    <Experience />
+    <TechStack />
+    <Testimonials />
+    <Contact />
+    <Footer />
+  </>
+);
+
+export default App;
