cd85134d9a08811346e74c65e2cef57e1b41ef40
Giang-Duc-Huy <giangduchuy537@gmail.com>
2026-06-15 13:09:48 +0700

    First commit

---
+++ myPortfolio/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1>My Portfolio</h1>
+      <p>Welcome to my portfolio website! Here you can find information about my projects, skills, and experience.</p>  
+    </div>
+  )
+}
+
+export default App
